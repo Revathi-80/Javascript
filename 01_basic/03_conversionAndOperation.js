f2fcd80c1c4c74f08da94fedfc6cdd4c9c428e1c
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-12-23 09:12:11 +0000

    03 conversion

--- 01_basic/03_conversionAndOperation.js
+++ 01_basic/03_conversionAndOperation.js
@@ -12,18 +12,34 @@ console.log(typeof valueInNumber); //number
 console.log(valueInNumber); //NaN
 console.log(tempInNumber); //0
 
-//"33" =>number
-//"33abc" =>value -> NaN   typeof ->number
-// true - 1  false -0
+// //"33" =>number
+// //"33abc" =>value -> NaN   typeof ->number
+// // true - 1  false -0
 
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
 
-// 1 -> true  0 -> false
-//"" -> false  "revathi" -> true
+// // 1 -> true  0 -> false
+// //"" -> false  "revathi" -> true
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber);
+
+/*********************************Operations*******************************/
+
+// The reason is that an equality check == and comparisons > < >= <= works differently
+// Comparisons convert null to a number, treating it as 0
+// that's why (3) null>= 0 true  and (1) null >0 is false
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined >= 0); //false
+
+//strict comparisons  ===
+console.log("2" === 2);
