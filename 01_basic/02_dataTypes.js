ddf95e7362a4610c7c97531e918668bb0199f470
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-12-23 09:40:45 +0000

    stack and heap

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -16,17 +16,17 @@ let state = null;
 //undefined
 //symbol =>unique
 
-// objects
+// // objects
 
-console.log(typeof "revathi");
-console.log(typeof null); // object
-console.log(typeof undefined); // undefined
+// console.log(typeof "revathi");
+// console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
 
-//symbol
+// //symbol
 
-const id = Symbol("123");
-const anotherId = Symbol("123");
-console.log(id === anotherId); //false
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+// console.log(id === anotherId); //false
 
 //primitive and non primitive
 // differenced by how data is stored in memeory and it is accessed
@@ -43,14 +43,40 @@ console.log(id === anotherId); //false
 //non- primitive / reference type (call by reference)
 //Array, Objects, Functions -> type -> Object function
 
-//array
-const heros = ["shaktiman", "naagraj", "doga"];
-//object
-let myObj = {
-  name: "revathi",
-  age: 12,
+// //array
+// const heros = ["shaktiman", "naagraj", "doga"];
+// //object
+// let myObj = {
+//   name: "revathi",
+//   age: 12,
+// };
+// //function
+// const myFunction = function () {
+//   console.log("Hello World");
+// };
+
+//********************************************Memory******************* */
+
+//Stack (primitive)
+// 1) it gives a copy
+
+//, Heap (Non -primitive)
+//1) it gives reference
+
+let Firstname = "Revathi";
+let anotherName = Firstname;
+anotherName = "P";
+
+console.log(Firstname); // revathi
+console.log(anotherName); //p
+
+let user1 = {
+  email: "user@gmail.com",
+  upi: "122121",
 };
-//function
-const myFunction = function () {
-  console.log("Hello World");
-};
+
+let user2 = user1;
+user2.email = "reavth@gmail.com";
+
+console.log(user1); ///{ email: 'reavth@gmail.com', upi: '122121' }
+console.log(user2); //{ email: 'reavth@gmail.com', upi: '122121' }
