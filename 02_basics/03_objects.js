7af626e35ce8e80e242c44381eb96de024f9d05b
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-12-23 14:04:30 +0000

    objects part1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,37 @@
+// constructor - singleton
+//  Object.create
+
+// declaring as literals - not a singleton
+//object literals
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+  name: "revathi",
+  //   mySym: "key1", //not valid way to add symbol
+  [mySym]: "key1",
+  "full name": "revathi p",
+  age: 18,
+  location: "chennai",
+  email: "revatgi@gmail.com",
+  lastLoginDays: ["monday", "friday"],
+};
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+
+JsUser.email = "revathi@gpt.com";
+Object.freeze(JsUser); // further changes on JsUser will not be made
+JsUser.email = "revathi@microsoft.com";
+console.log(JsUser);
+// {
+//     name: 'revathi',
+//     'full name': 'revathi p',
+//     age: 18,
+//     location: 'chennai',
+//     email: 'revathi@gpt.com',
+//     lastLoginDays: [ 'monday', 'friday' ],
+//     [Symbol(key1)]: 'key1'
+//   }
