8502d9cc823eb931482349ccaabe4ec98c8fdd7e
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2023-12-23 09:27:50 +0000

    data type summary

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -21,3 +21,36 @@ let state = null;
 console.log(typeof "revathi");
 console.log(typeof null); // object
 console.log(typeof undefined); // undefined
+
+//symbol
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+console.log(id === anotherId); //false
+
+//primitive and non primitive
+// differenced by how data is stored in memeory and it is accessed
+//Javscript s dynamically typed
+
+// -----------------------------------------------------------------------------------------------------
+
+// primitive (call by value) (copy)
+//7 -> String,Number , boolean, Null, undefined, Symbol (to make the value unique), BigInt
+// tyeof BigInt-> undefined
+
+// -------------------------------------------------------------------------------------------------------
+
+//non- primitive / reference type (call by reference)
+//Array, Objects, Functions -> type -> Object function
+
+//array
+const heros = ["shaktiman", "naagraj", "doga"];
+//object
+let myObj = {
+  name: "revathi",
+  age: 12,
+};
+//function
+const myFunction = function () {
+  console.log("Hello World");
+};
